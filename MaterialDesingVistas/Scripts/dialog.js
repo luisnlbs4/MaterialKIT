7e93c79681b395774d98df1855a4c1336b6d96d4
Luis Veizaga Delgado <luisnlbs4@gmail.com>
2018-12-18 17:18:33 -0400

    acabando de hacer los add de reduction tickets y fees

--- MaterialDesingVistas/Scripts/dialog.js
+++ MaterialDesingVistas/Scripts/dialog.js
@@ -1,11 +1,26 @@
 var dialog = document.querySelector('dialog');
+var dialogadd = document.querySelector('.dialogAdd');
 var showDialogButton = document.querySelector('#show-dialog');
+var showDialogButtonAdd = document.querySelector('#show-dialogadd');
+
 if (! dialog.showModal) {
   dialogPolyfill.registerDialog(dialog);
+}
+if (! dialogadd.showModal) {
+  dialogPolyfill.registerDialog(dialogadd);
 }
 showDialogButton.addEventListener('click', function() {
   dialog.showModal();
 });
+
+showDialogButtonAdd.addEventListener('click', function() {
+  dialogadd.showModal();
+});
+
+dialogadd.querySelector('.close').addEventListener('click', function() {
+  dialogadd.close();
+});
+
 dialog.querySelector('.close').addEventListener('click', function() {
   dialog.close();
 });
